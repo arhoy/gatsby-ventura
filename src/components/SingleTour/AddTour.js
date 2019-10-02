a619005ad6e254a5a1d7ceb4ba2f73d0d639665f
arhoy <arhoy@ualberta.ca>
2019-10-02 16:03:29 -0600

    fix slug agsing

--- src/components/SingleTour/AddTour.js
+++ src/components/SingleTour/AddTour.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../../scss/components/SingleTour/AddTour.module.scss';
 
 const AddTour = ({ name, price, id, startDate, src, slug }) => {
-  console.info('tour added')
+  console.info('tour added',slug)
   return (
     <button
       className={`snipcart-add-item ${styles.button}`}
@@ -11,7 +11,7 @@ const AddTour = ({ name, price, id, startDate, src, slug }) => {
       data-item-price={price}
       data-item-startDate={startDate}
       data-item-image={src}
-      data-item-url={`https://ventura-gatsby-site-101.netlify.com/tours${slug}`}
+      data-item-url={`https://ventura-gatsby-site-101.netlify.com/tours/${slug}`}
     >
       Add to Cart
     </button>
