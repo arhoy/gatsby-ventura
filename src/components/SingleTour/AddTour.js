5395ce6fef3cfce928895a1f7820fa6d0f589e75
arhoy <arhoy@ualberta.ca>
2019-10-02 15:13:19 -0600

    changed url for snip cart

--- src/components/SingleTour/AddTour.js
+++ src/components/SingleTour/AddTour.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from '../../scss/components/SingleTour/AddTour.module.scss';
 
 const AddTour = ({ name, price, id, startDate, src }) => {
+  console.info('tour added')
   return (
     <button
       className={`snipcart-add-item ${styles.button}`}
@@ -10,7 +11,7 @@ const AddTour = ({ name, price, id, startDate, src }) => {
       data-item-price={price}
       data-item-startDate={startDate}
       data-item-image={src}
-      data-item-url="https://ventura-gatsby-site-101.netflify.com/tours"
+      data-item-url="https://ventura-gatsby-site-101.netlify.com/tours"
     >
       Add to Cart
     </button>
